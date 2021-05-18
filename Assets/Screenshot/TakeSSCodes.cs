d2a7764918f0d6ae5c5142d4b3bb047d0e9094c4
BerkayOytunYilmaz <brkylmz12@gmail.com>
2021-05-18 21:28:07 +0300

    Ekran Görüntüsü Alma Özelliği Geliştirildi
    Farklı çözünürlüklerde istenilen konuma istenilen kamera üzerinden ekran görüntüsü alıp kaydeden fonksiyon yazıldı ve test edildi.

---
+++ Assets/Screenshot/TakeSSCodes.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeSSCodes : MonoBehaviour
+{
+    public static TakeSSCodes instance;
+
+    private bool takeSSOnNextFrame = false;
+    
+    private Camera cam;
+    private string path;
+    private string fileName;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void OnPostRender()
+    {
+        if (takeSSOnNextFrame)
+        {
+            takeSSOnNextFrame = false;
+
+            RenderTexture renderTexture = cam.targetTexture;
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes(path + "/" + fileName + ".png", byteArray);
+            Debug.Log("Screenshot saved.");
+
+            RenderTexture.ReleaseTemporary(renderTexture);
+            cam.targetTexture = null;
+        }
+    }
+
+    private void TakeScreenshot(Camera cam, int width, int height, string path, string fileName)
+    {
+        cam.targetTexture = RenderTexture.GetTemporary(width, height, 16);
+        this.cam = cam;
+        this.path = path;
+        this.fileName = fileName;
+        takeSSOnNextFrame = true;
+    }
+
+    public static void TakeSS(Camera cam, int width, int height, string path, string fileName)
+    {
+        instance.TakeScreenshot(cam, width, height, path, fileName);
+    }
+}
---
+++ Assets/Screenshot/TestTakeSS.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTakeSS : MonoBehaviour
+{
+    public string path;
+    public Camera ssCam;
+    public Vector2Int resulution;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeSSCodes.TakeSS(ssCam, resulution.x, resulution.y, path, "deneme");
+        }
+    }
+}
